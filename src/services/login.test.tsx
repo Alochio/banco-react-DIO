f2a0bc0a0409139356f14bf1b5f4ddad258f8e65
Alochio <vinicius.alochio@aluno.ufop.edu.br>
2024-03-12 12:52:34 -0300

    teste unitário completo

--- src/services/login.test.tsx
+++ src/services/login.test.tsx
@@ -3,7 +3,7 @@ import { login } from "./login";
 describe("login", () => {
   const mockAlert = jest.fn();
   window.alert = mockAlert;
-
+  
   it("Deve exibir um alert com boas vindas", () => {
     login();
     expect(mockAlert).toHaveBeenLastCalledWith("Bem vindo!");
